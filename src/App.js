749f79193442b34c52bd9451216d0eb5651b482d
Michael Watts <wattsjmichael@gmail.com>
2025-05-27 11:24:59 -0700

    Moved to React

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <nav className="navbar">
+        <h1>DadBod Studios</h1>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Games</a></li>
+          <li><a href="#">Minigrants</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+      <main>
+        <h2>Welcome to DadBod Studios</h2>
+        <p>We make claymation shooters and fund wild indie games. Our debut title <strong>Kiln</strong> is coming soon.</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
